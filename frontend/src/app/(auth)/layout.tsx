b304c00403ba68677c3f03376279ddcfda722fe4
24LP9559 24LP9559 <24LP9559@G301-CC-206.local>
2025-05-07 12:37:36 +0800

    a

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,9 +1,11 @@
+import { Icon } from "@/components/Icon";
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen">
-      <div className="w-[40%] h-full flex items-center justify-end pr-12">
+    <div className="flex h-screen bg-[#FBBF24] relative">
+      <Icon className="absolute" />
+      <div className="w-[50%] bg-white h-full flex items-center justify-end pr-12">
         {children}
       </div>
     </div>
