565251f5323e23ee44c5c04d8fcf3f7c51e48e6e
ganaa037 <tuvaangantumur@gmail.com>
2025-05-16 00:17:01 +0800

    signup form

--- frontend/src/app/(auth)/sign-up/Step2.tsx
+++ frontend/src/app/(auth)/sign-up/Step2.tsx
@@ -1,15 +1,24 @@
 import {
+  Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import React from "react";
+import React, { useState } from "react";
 import { FormInput } from "../_component/FormInput";
+import { Button } from "@/components/ui/button";
 
 export const Step2 = () => {
+  const [email, setEmail] = useState<string>()
+  const [password, setPassword] = useState<string>()
+  console.log(email, password);
+
+
   return (
-    <div className="">
+
+    <Card className="w-[407px] h-[256px] border-none shadow-none">
       <CardHeader className="flex flex-col gap-[6px] p-6">
         <CardTitle>Welcome, baconpancakes1</CardTitle>
         <CardDescription>Connect email and set a password</CardDescription>
@@ -20,14 +29,22 @@ export const Step2 = () => {
           type="email"
           placeholder="Enter email here"
           error=""
+          setChange={setEmail}
         />
         <FormInput
           label="password"
           type="password"
           placeholder="Enter password here"
           error=""
+          setChange={setPassword}
         />
       </CardContent>
-    </div>
+      <CardFooter className="flex justify-between">
+        <Button className="w-full">
+          Continue
+        </Button>
+      </CardFooter>
+    </Card>
+
   );
 };
