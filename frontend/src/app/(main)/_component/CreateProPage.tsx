7b6db4f95e57ab39faae0c8045cae7e47225d492
24LP9559 24LP9559 <24LP9559@G301-CC-206.local>
2025-05-16 09:29:10 +0800

    propage

--- frontend/src/app/(main)/_component/CreateProPage.tsx
+++ frontend/src/app/(main)/_component/CreateProPage.tsx
@@ -1,45 +1,55 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
 import { z } from "zod";
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Step1 } from "@/app/(auth)/sign-up/Step1";
 
 const formSchema = z.object({
-  names: z.string({ required_error: "ner oruul" }).min(2, { message: "2 oos deesh useg orno" }),
-  photo: z.string({ required_error: "zurag oruul" }).min(1, { message: "zurgaa oruulna uu" }),
+  names: z
+    .string({ required_error: "ner oruul" })
+    .min(2, { message: "2 oos deesh useg orno" }),
+  photo: z
+    .string({ required_error: "zurag oruul" })
+    .min(1, { message: "zurgaa oruulna uu" }),
   about: z.string().min(1, { message: "Please enter info about yourself" }),
-  URL: z.string().min(1, { message: "Please enter a social link" })
-})
+  URL: z.string().min(1, { message: "Please enter a social link" }),
+});
 
 export const CreateproPage = () => {
-
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       names: "",
       photo: "",
       about: "",
-      URL: ""
+      URL: "",
     },
-  })
+  });
 
   const handleSubmit = (values: z.infer<typeof formSchema>) => {
     console.log(values);
-    Step1()
-  }
+    Step1();
+  };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmit)} >
+      <form onSubmit={form.handleSubmit(handleSubmit)}>
         <div className="flex justify-center">
           <div className="w-[510px] h-fit border ">
             <div className="flex gap-6 flex-col justify-end">
               <p>Complete your profile page</p>
-
 
               <FormField
                 control={form.control}
@@ -48,11 +58,14 @@ export const CreateproPage = () => {
                   <FormItem>
                     <FormLabel>Add photo </FormLabel>
                     <FormControl>
-                      <Input className="rounded-[9999px] size-[160px] border border-black text-transparent"
-                        type="file" placeholder="shadcn" {...field} />
+                      <Input
+                        className="rounded-[9999px] size-[160px] border border-black text-transparent"
+                        type="file"
+                        placeholder="shadcn"
+                        {...field}
+                      />
                     </FormControl>
-                    <FormDescription>
-                    </FormDescription>
+                    <FormDescription></FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
@@ -64,10 +77,13 @@ export const CreateproPage = () => {
                   <FormItem>
                     <FormLabel>Name</FormLabel>
                     <FormControl>
-                      <Input className="w-full h-10 border" placeholder="shadcn" {...field} />
+                      <Input
+                        className="w-full h-10 border"
+                        placeholder="shadcn"
+                        {...field}
+                      />
                     </FormControl>
-                    <FormDescription>
-                    </FormDescription>
+                    <FormDescription></FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
@@ -80,10 +96,13 @@ export const CreateproPage = () => {
                   <FormItem>
                     <FormLabel>About</FormLabel>
                     <FormControl>
-                      <Input className="h-[131px] w-full border" placeholder="shadcn" {...field} />
+                      <Input
+                        className="h-[131px] w-full border"
+                        placeholder="shadcn"
+                        {...field}
+                      />
                     </FormControl>
-                    <FormDescription>
-                    </FormDescription>
+                    <FormDescription></FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
@@ -96,16 +115,20 @@ export const CreateproPage = () => {
                   <FormItem>
                     <FormLabel>Social media URL</FormLabel>
                     <FormControl>
-                      <Input className="h-10 w-full border"
-                        placeholder="https://" {...field} />
+                      <Input
+                        className="h-10 w-full border"
+                        placeholder="https://"
+                        {...field}
+                      />
                     </FormControl>
-                    <FormDescription>
-                    </FormDescription>
+                    <FormDescription></FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-              <Button className="w-[246px] " type="submit" >Continue</Button>
+              <Button className="w-[246px] " type="submit">
+                Continue
+              </Button>
             </div>
           </div>
         </div>
