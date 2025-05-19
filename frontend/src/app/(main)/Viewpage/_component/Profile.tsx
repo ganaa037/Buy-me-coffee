89f8dec97a860375ee00c0b17bbbb70c204cc563
ganaa037 <tuvaangantumur@gmail.com>
2025-05-19 22:35:21 +0800

    viewpage ursun

--- frontend/src/app/(main)/Viewpage/_component/Profile.tsx
+++ frontend/src/app/(main)/Viewpage/_component/Profile.tsx
@@ -1,14 +1,15 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Coffee, Heart } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 export const Profile = () => {
   return (
-    <div className="flex gap-5 px-20">
-      <div className="w-[632px] h-fit gap-5 flex flex-col">
-        <div className=" p-6 border flex flex-col rounded-[8px] gap-4">
+    <div className="flex gap-5 px-20 h-screen items-center">
+      <div className="w-1/2 h-fit gap-5 flex flex-col">
+        <div className=" p-6 border flex flex-col rounded-[8px] gap-4 bg-white">
           <div className="flex justify-between">
             <div className="flex gap-3">
               <Avatar>
@@ -31,9 +32,9 @@ export const Profile = () => {
           <p>Social media URL</p>
           <p>https://buymeacoffee.com/spacerulz44</p>
         </div>
-        <div className="border rounded-[8px] p-6 gap-[18px]">
+        <div className="border rounded-[8px] p-6 gap-[18px] flex w-full h-fit flex-col">
           <p>Recent Supporters</p>
-          <div className="w-[584px] p-6 flex flex-col items-center">
+          <div className="w-full h-fit p-6 flex  items-center">
             <div className="p-[17.78px]">
               <Heart className="" />
             </div>
@@ -41,15 +42,30 @@ export const Profile = () => {
           </div>
         </div>
       </div>
-      <div className="w-[628px] border">
-        <p>Buy Jake a Coffee</p>
-        <div className="flex">
-          <p>Select amount</p>
-          <div>
-            <Coffee />
-            <p>$1S</p>
+      <div className="bg-white w-1/2 border p-6 flex gap-8 flex-col h-fit rounded-[8px]">
+        <div className="flex gap-6 flex-col">
+          <p>Buy Jake a Coffee</p>
+          <div className="flex flex-col ">
+            <p>Select amount:</p>
+            <div className="flex gap-4 ">
+              <Button variant={"secondary"}><Coffee />$1</Button>
+              <Button variant={"secondary"}> <Coffee />$2</Button>
+              <Button variant={"secondary"}><Coffee />  $5</Button>
+              <Button variant={"secondary"}><Coffee />$10</Button>
+            </div>
           </div>
         </div>
+
+        <div className="gap-5 flex flex-col">
+          <p>Enter BuyMeCoffee or social acount URL:</p>
+          <Input placeholder="buymeacoffee.com/" />
+
+          <div>
+            <p>Special message:</p>
+            <Textarea className="h-[131px]" placeholder="Please write your message here" />
+          </div>
+        </div>
+        <Button>Support</Button>
       </div>
     </div>
   );
--- frontend/src/app/(main)/Viewpage/page.tsx
+++ frontend/src/app/(main)/Viewpage/page.tsx
@@ -3,7 +3,8 @@ import { Profile } from "./_component/Profile";
 
 export default function page() {
   return (
-    <div>
+    <div className="h-[319px] w-full bg-amber-500">
+
       <Profile />
     </div>
   );
