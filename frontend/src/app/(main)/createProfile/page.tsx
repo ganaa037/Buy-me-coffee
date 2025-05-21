eb5983602388eb3b1bd9584dc7dcbc3d57a36c39
ganaa037 <tuvaangantumur@gmail.com>
2025-05-21 23:39:04 +0800

    select country (#28)
    Co-authored-by: 24LP9559 24LP9559 <24LP9559@G301-CC-206.local>

--- frontend/src/app/(main)/createProfile/page.tsx
+++ frontend/src/app/(main)/createProfile/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { CreateProNav } from "../_component/CreateProNav";
+import { PageSelectCountry } from "../_component/PageSelectCountry";
 import { CreateproPage } from "../_component/CreateProPage";
 
 export default function page() {
   return (
     <div className="flex flex-col gap-[91px]">
       <CreateProNav />
+      <PageSelectCountry />
       <CreateproPage />
     </div>
   );
