667178df919259edd9677d5a354cf1aff4725a7d
ganaa037 <tuvaangantumur@gmail.com>
2025-05-21 23:10:08 +0800

    account setting ursun

---
+++ frontend/src/app/(main)/accountSetting/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Setting } from './_component/Setting'
+
+const page = () => {
+    return (
+        <div>
+            <Setting />
+        </div>
+    )
+}
+
+export default page
