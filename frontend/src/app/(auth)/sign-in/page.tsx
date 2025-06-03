abaec5189387c6e4be5ec9f741fb0d4e45c1db63
ganaa037 <tuvaangantumur@gmail.com>
2025-06-03 10:49:25 +0800

    sign-in (#25)
    * sign-in
    
    * signin update

--- frontend/src/app/(auth)/sign-in/page.tsx
+++ frontend/src/app/(auth)/sign-in/page.tsx
@@ -7,8 +7,23 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import { Button } from '@/components/ui/button'
-import { FormInput } from '../_component/FormInput'
 import { useRouter } from "next/navigation"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+const formSchema = z.object({
+    email: z.string().min(2, { message: "Please enter a valid email" }),
+    password: z.string().min(8, { message: "" })
+})
 export default function Page() {
     // const [change, setChange] = useState("")
     // setChange(change)
@@ -16,6 +31,64 @@ export default function Page() {
     const handleClick = () => {
         router.push("sign-up")
     }
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+    })
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
+    return (
+        <div className="h-screen gap-[248px] flex-col flex ">
+            <div className="w-full justify-end flex pt-8">
+                <Button onClick={handleClick}>
+                    Sign up
+                </Button>
+            </div>
+            <Card className="w-[407px] h-fit border-none shadow-none">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <CardHeader className="flex flex-col gap-[6px] p-6">
+                            <CardTitle>Welcome back</CardTitle>
+                        </CardHeader>
+                        <CardContent className='flex flex-col pb-6 gap-3'>
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input type="email" placeholder="Enter email here" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input type="password" placeholder="Enter password here" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </CardContent>
+                        <CardFooter className="flex justify-between">
+                            <Button className="w-full">Deploy</Button>
+                        </CardFooter>
+                    </form>
+                </Form>
+            </Card>
+        </div>
     const handleOnclick = () => {
         router.push("createProfile")
     }
